fd0195b0d4ef9c9be6af3900b12695e58b342662
anton <ziick1234@live.se>
2021-12-19 17:58:12 +0100

    C#

---
+++ ConsoleApp1/Julbord.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Julbord
+    {
+        public static void AddVisitor()
+        {
+            Visitor Visitor = new Visitor();
+
+            Console.WriteLine("Förnamn:");
+            Visitor.FirstName = Console.ReadLine();
+            Console.WriteLine("\r");
+
+            Console.WriteLine("Efternamn:");
+            Visitor.LastName = Console.ReadLine();
+            Console.WriteLine("\r");
+
+            Console.WriteLine("Epost:");
+            Visitor.Email = Console.ReadLine();
+            Console.WriteLine("\r");
+
+            Console.WriteLine("Ålder:");
+            Visitor.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\r");
+
+            File.AppendAllText(@"visitors.txt", $"{Visitor.FirstName} {Visitor.LastName}, {Visitor.Email}, {Visitor.Age} {Environment.NewLine}");
+
+        }
+
+
+        public void Remove()
+        {
+            Visitors();
+            Console.WriteLine("\r");
+
+            Console.WriteLine("Skriv Förnamn och Efternamn på besökaren du vill ta bort.:");
+            string strFilePath = "visitors.txt";
+            string strSearchText = Console.ReadLine();
+            string strOldText;
+            string n = "";
+            StreamReader sr = File.OpenText(path: strFilePath);
+            while ((strOldText = sr.ReadLine()) != null)
+            {
+                if (!strOldText.Contains(strSearchText))
+                {
+                    n += strOldText + Environment.NewLine;
+                }
+            }
+            sr.Close();
+            File.WriteAllText(strFilePath, n);
+            Console.WriteLine("\r");
+
+            Console.WriteLine(strSearchText + " Har blivit bortplockad från julbordet </3 :(");
+        }
+
+        public static void Visitors()
+        {
+            string file = @"visitors.txt";
+
+            if (File.Exists(file))
+            {
+                string text = File.ReadAllText(file);
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine("Inga besökare registrerade på julbordet");
+            }
+        }
+
+        public void Discount()
+        {
+            Console.WriteLine("Med denna får du 20% på valfri dryck eller rätt");
+            Console.WriteLine(@"Rabattkoden är" + " " + Guid.NewGuid() + ".");
+        }
+
+        readonly string menu = @"
+                Lägg till                  Lägg till en besökare i listan
+                Ta bort                    Ta bort en besökare från listan
+                Visa                       Visar alla besökare i listan
+                Rabatt <3                  Få din rabattkod
+                Meny                       Se Menyvalen
+                Avsluta                    Avslutar programmet";
+
+        public void Menu()
+        {
+            Console.WriteLine(menu);
+        }
+
+        public void Exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Julbord Show = new();
+
+            Console.WriteLine("\r\n \r\n");
+            Console.WriteLine("            Välkommen till julbordet! \r\n");
+            Console.WriteLine("            Välj ett alternativ:");
+            Console.WriteLine(@"
+                Lägg till                  Lägg till en besökare i listan
+                Ta bort                    Ta bort en besökare från listan
+                Visa                       Visar alla besökare i listan
+                Rabatt <3                  Få din rabattkod
+                Meny                       Se Menyvalen
+                Avsluta                    Avslutar programmet");
+
+            Console.Write("\r\n \r\n >");
+
+            string UserInput = Console.ReadLine();
+
+            while (!UserInput.Equals("") && !UserInput.ToLower().Equals("exit"))
+            {
+                if (UserInput.StartsWith("Lägg till") || UserInput.StartsWith("lägg till"))
+                {
+                    Julbord.AddVisitor();
+                }
+                else if (UserInput.StartsWith("Ta bort") || UserInput.StartsWith("ta bort"))
+                {
+                    Show.Remove();
+                }
+                else if (UserInput.StartsWith("Visa") || UserInput.StartsWith("visa"))
+                {
+                    Julbord.Visitors();
+                }
+                else if (UserInput.StartsWith("Rabatt <3") || UserInput.StartsWith("rabatt <3") || UserInput.StartsWith("Rabatt") || UserInput.StartsWith("rabatt"))
+                {
+                    Show.Discount();
+                }
+                else if (UserInput.StartsWith("Menu") || UserInput.StartsWith("meny"))
+                {
+                    Show.Menu();
+                }
+                else if (UserInput.StartsWith("Avsluta") || UserInput.StartsWith("avsluta"))
+                {
+                    Show.Exit();
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt kommando, försök igen!");
+                }
+                Console.Write("\r\n \r\n >");
+                UserInput = Console.ReadLine();
+            }
+        }
+    }
+}
